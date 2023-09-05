2d5ef1d4709a06e118dc2356bd9956c7d9e86bae
surenther <suenthiran0604@gmial.com>
2023-09-05 08:43:00 +0530

    5/9/23/morning

---
+++ application/controllers/Mentors.php
@@ -0,0 +1,21 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mentors  extends CI_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->library('session');
+        $this->load->model('Mentors_model');
+    }
+
+    public function index() {
+        $mentors = $this->Mentorsmpdel->show();
+        $data['mentors'] = $mentors;
+        $this->load->view('admin/mentor/list', $data);
+    }
+}
+
+    ?>
---
+++ application/views/admin/mentors/list.php
@@ -0,0 +1,49 @@
+<?php $this->view('admin/includes/header'); ?>
+<div class="page-header d-flex justify-content-between">
+    <div class="page-leftheader">
+        <h4 class="page-title mb-0 text-primary">mentors</h4>
+    </div>
+    <div class="page-rightheader">
+        <div class="btn-list">
+            <a href="<?php echo base_url(); ?>admin/mentors/add" class="btn btn-primary"><i class="fe fe-plus"></i> Add Details</a>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="">
+                    <table class="CustomersTable table table-bordered table-striped table-responsive" style="width: 100%">
+                        <thead>
+                            <tr>
+                                <th style="width: 10%">S.No</th>
+                                <th style="width: 20%">Course Name</th>
+                                <th style="width: 20%">Short Notes</th>
+                                <th style="width: 30%">Image</th>
+                                <th style="width: 10%">Details Links</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php //foreach ($customers as $key => $item) { ?>
+                                <tr>
+                                    <td><?php // echo ($key + 1); ?></td>
+                                    <td><?php //echo $item['course_name']; ?></td>
+                                    <td><?php // echo $item['short_note']; ?></td>
+                                    <td><?php //echo $item['image']; ?></td>
+                                    <td>
+                                        <a href="<?php //echo base_url() . 'customers/edit/' . $item['id']; ?>" class="btn btn-icon btn-warning"><i class="fe fe-edit"></i></a>
+                                        <button type="button" class="remove-customer-btn btn btn-icon btn-danger" value="<?php // echo $item['id']; ?>"><i class="fe fe-trash"></i></button>
+                                    </td>
+                                </tr>
+                            <?php // } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php $this->view('admin/includes/footer'); ?>
+<script src="<?php echo base_url(); ?>admin_assets/app/js/customers/list.js?version=<?php echo uniqid(); ?>"></script>
+<script src="<?php // echo base_url(); ?>admin_assets/app/js/customers/delete.js?version=<?php echo uniqid(); ?>"></script>
